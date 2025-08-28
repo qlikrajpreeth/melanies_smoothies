4ba643038bff3741b10026c86abd6b9e9604f0e3
qlikrajpreeth <srajpreeth@gmail.com>
2025-08-28 21:54:54 +0530

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,8 @@ if ingredients_list:
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
         #st.write(my_insert_stmt)
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
